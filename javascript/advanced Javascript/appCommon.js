9668882be52dc1b4215dc4c97710c1e7ffe5897e
Mohit <mohit750gandhi@gmail.com>
2026-02-18 11:30:42 +0530

    es6 modules and CommonJs

---
+++ javascript/advanced Javascript/appCommon.js
@@ -0,0 +1,3 @@
+const mathoperation = require("./mathoperationcommon.js")
+
+console.log(mathoperation.add(4,5));
---
+++ javascript/advanced Javascript/appM.js
@@ -0,0 +1,6 @@
+/* Default import */
+import multiply from "./mathOperationsM.js";
+
+import {add,subtract} from "./mathOperationsM.js"
+console.log(multiply(4,5))
+console.log(add(2,2));
