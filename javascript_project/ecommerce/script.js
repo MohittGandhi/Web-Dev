a89f4e54e8973e54fa8fda67667111a2ba2a1a02
Mohit <mohit750gandhi@gmail.com>
2026-02-23 23:42:24 +0530

    ecommerce update

--- javascript_project/ecommerce/script.js
+++ javascript_project/ecommerce/script.js
@@ -1,10 +1,9 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener("DOMContentLoaded", () => {
   const products = [
-    {id:1,name:"Product 1",price: 29.5},
-    {id:2,name:"Product 2",price: 34.9},
-    {id:3,name:"Product 3",price: 40.9},
-  ]
-
+    { id: 1, name: "Product 1", price: 29.5 },
+    { id: 2, name: "Product 2", price: 34.9 },
+    { id: 3, name: "Product 3", price: 40.9 },
+  ];
 
   const cart = [];
 
@@ -15,12 +14,44 @@ document.addEventListener('DOMContentLoaded',()=>{
   const totalPriceDisplay = document.getElementById("total-price");
   const checkOutBtn = document.getElementById("checkout-btn");
 
-  products.forEach((product) =>{
+  products.forEach((product) => {
     const productDiv = document.createElement("div");
     productDiv.classList.add("product");
-    productDiv.innerHTML= 
-    `<span>${productDiv.name} - $ ${productDiv.price.toFixed(2)}</span>
-    <button data-id = "${product.id}"> Add to cart</button>`;
+    productDiv.innerHTML = `<span>${product.name} - $ ${product.price.toFixed(2)}</span>
+    <button data-id ="${product.id}"> Add to cart</button>`;
     productList.appendChild(productDiv);
-  })
-})
+  });
+
+  productList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const productId = parseInt(e.target.getAttribute("data-id"));
+      const product = products.find(p=>p.id === productId);
+      addToCart(product);//console.log(product);
+    }
+  });
+
+  function addToCart(product){
+    cart.push(product);
+    renderCart();
+  };
+
+  function renderCart(){
+    cartItems.innerText = "";
+    let totalPrice = 0;
+    if(cart.length >0){
+      emptyCartMessage.classList.add("hidden");
+      cartTotalMessage.classList.remove("hidden");
+      cart.forEach((items,index)=>{
+        totalPrice += items.price;
+        const cartItem = document.createElement('div')
+        cartItem.innerHTML = `
+        ${items.name} -$ ${items.price}
+        `;
+        cartItems.appendChild(cartItem);
+        totalPriceDisplay.textContent = `${totalPrice.toFixed(2)}`;
+      })
+    }else{
+      emptyCartMessage.classList.remove("hidden");
+    }
+  }
+});
