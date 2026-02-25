d423e552584361c92739ddbe2bf719086bcff93d
Mohit <mohit750gandhi@gmail.com>
2026-02-26 02:08:58 +0530

    expense tracker

--- javascript_project/expense_tracker/script.js
+++ javascript_project/expense_tracker/script.js
@@ -1,35 +1,70 @@
-document.addEventListener('DOMContentLoaded',(()=>{
+document.addEventListener("DOMContentLoaded",()=>{
   const expenseForm = document.getElementById("expense-form");
   const expenseNameInput = document.getElementById("expense-name");
   const expenseAmountInput = document.getElementById("expense-amount");
   const expenseList = document.getElementById("expense-list");
-  const totalAmountDisplay = document.getElementById("expense-list");
+  const totalAmountDisplay = document.getElementById("total-amount");
 
-  let expense = []
+  let expenses =JSON.parse(localStorage.getItem("expense")) || [];
 
-  let totalAmount = CalculateTotal()
+  let totalAmount = CalculateTotal();
+
+  renderExpenses();
 
   expenseForm.addEventListener("submit",(e) =>{
-    e.preventDefault()
+    e.preventDefault(); 
     const name = expenseNameInput.value.trim();
     const amount = parseFloat(expenseAmountInput.value.trim());
-    if(name !== "" && isNaN(amount) && amount >0){
+    if(name !== "" && !isNaN(amount) && amount >0){
       const newExpense = {
         id: Date.now(),
         name: name,
         amount: amount,
       }
-      expense.push(newExpense);
+      expenses.push(newExpense);
       saveExpensesTolocal();
+      renderExpenses();
+      uploadtotal();
+      localStorage.getItem("expense");
+
+      expenseNameInput.value = "";
+      expenseNameInput.value = "";
     }
   });
     
-  function CalculateTotal() {
 
+  function renderExpenses(){
+    expenseList.innerHTML= "";
+    expenses.forEach((expense) =>{
+      const li = document.createElement("li");
+      li.innerHTML= `${expense.name} - $${expense.amount} <button data-id = "${expense.id}"> Delete </button>`;
+      expenseList.append(li);
+    })
+  }
+  
+
+  function CalculateTotal() {
+    return expenses.reduce((sum,expense) => sum + expense.amount,0);
   }  
 
   function saveExpensesTolocal(){
-    localStorage.setItem("expense ",JSON.stringify(expense))
+    localStorage.setItem("expense",JSON.stringify(expenses))
   }
 
-}))
+  function uploadtotal(){
+    totalAmount = CalculateTotal();
+    totalAmountDisplay.textContent = totalAmount.toFixed(2);
+  }
+
+  expenseList.addEventListener('click',(e) =>{
+  if(e.target.tagName === "BUTTON"){
+    const expenseid = parseInt(e.target.getAttribute("data-id"));
+    expenses = expenses.filter((expense) =>expense.id !== expenseid );
+
+    saveExpensesTolocal();
+    renderExpenses();
+    uploadtotal();
+  }
+  })
+
+});
